077a26167bd29af4708ed9ad4508b5dd94df578a
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-10-05 08:47:11 +0200

    Convert copyright statements to SPDX expressions

--- abstractlocker.h
+++ abstractlocker.h
@@ -1,27 +1,13 @@
-/********************************************************************
- KSld - the KDE Screenlocker Daemon
- This file is part of the KDE project.
+/*
+    SPDX-FileCopyrightText: 1999 Martin R. Jones <mjones@kde.org>
+    SPDX-FileCopyrightText: 2002 Luboš Luňák <l.lunak@kde.org>
+    SPDX-FileCopyrightText: 2003 Oswald Buddenhagen <ossi@kde.org>
+    SPDX-FileCopyrightText: 2008 Chani Armitage <chanika@gmail.com>
+    SPDX-FileCopyrightText: 2011 Martin Gräßlin <mgraesslin@kde.org>
+    SPDX-FileCopyrightText: 2015 Bhushan Shah <bhush94@gmail.com>
 
- Copyright (C) 1999 Martin R. Jones <mjones@kde.org>
- Copyright (C) 2002 Luboš Luňák <l.lunak@kde.org>
- Copyright (C) 2003 Oswald Buddenhagen <ossi@kde.org>
- Copyright (C) 2008 Chani Armitage <chanika@gmail.com>
- Copyright (C) 2011 Martin Gräßlin <mgraesslin@kde.org>
- Copyright (C) 2015 Bhushan Shah <bhush94@gmail.com>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef ABSTRACTLOCKER_H
 #define ABSTRACTLOCKER_H
