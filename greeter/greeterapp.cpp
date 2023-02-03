8486e95bb63170fe450d9ca44bbf06dbc029114f
David Edmundson <kde@davidedmundson.co.uk>
2023-02-03 23:09:51 +0000

    drop unused deprecated include

--- greeter/greeterapp.cpp
+++ greeter/greeterapp.cpp
@@ -25,7 +25,6 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include <KLocalizedContext>
 #include <KScreenDpms/Dpms>
 #include <KWindowSystem>
-#include <kdeclarative/kdeclarative.h>
 
 #include <KUser>
 // Plasma
