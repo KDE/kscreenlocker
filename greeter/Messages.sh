9cb757a0ac523d71912407ac1815bac284b45af3
Harald Sitter <sitter@kde.org>
2025-07-21 07:13:28 +0200

    greeter: extract qml i18n as well
    replace the entire file with a more modern version
    
    BUG: 507240

--- greeter/Messages.sh
+++ greeter/Messages.sh
@@ -1,2 +1,13 @@
-#!bin/sh
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kscreenlocker_greet.pot
+#!/bin/sh
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2020-2025 Harald Sitter <sitter@kde.org>
+
+# Ensure passing shellcheck:
+# Our l10n scripting isn't working with spaces anywhere and we actively rely on word splitting in our Messages.sh.
+# shellcheck disable=SC2046
+podir=${podir:?} # ensure it is defined
+
+$XGETTEXT $(find . -name \*.cpp -o -name \*.h) -o "$podir"/kscreenlocker_greet.pot
+# Extract JavaScripty files as what they are, otherwise for example template literals won't work correctly (by default we extract as C++).
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+$XGETTEXT --join-existing --language=JavaScript $(find . -name \*.qml -o -name \*.js) -o "$podir"/kscreenlocker_greet.pot
