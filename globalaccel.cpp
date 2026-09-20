f10b596cd43afa12057458a2bd6e29fa75f3e265
onemorepash <plunin@gmail.com>
2026-09-20 11:23:54 +0200

    Update globalaccel.cpp
    Adding "Switch to Last-Used Keyboard Layout" to the shortcut exceptions list.
    
    Switch to Last-Used Keyboard Layout was added in Plasma 6 to support switching between the 2 last layouts when more than 2 layouts are configured: https://discuss.kde.org/t/bug-fix-fix-spare-layout-feature-on-wayland/5525/5

--- globalaccel.cpp
+++ globalaccel.cpp
@@ -32,7 +32,7 @@ static const QMap<QString, QRegularExpression> s_shortcutWhitelist{
      QRegularExpression(QStringLiteral("Increase Screen Brightness|Decrease Screen Brightness|Increase Keyboard Brightness|Decrease Keyboard Brightness|Toggle "
                                        "Keyboard Backlight|Turn Off Screen|Sleep|Hibernate"))},
     {QStringLiteral("/component/KDE_Keyboard_Layout_Switcher"),
-     QRegularExpression(QStringLiteral("Switch to Next Keyboard Layout|Switch keyboard layout to .*"))},
+     QRegularExpression(QStringLiteral("Switch to Next Keyboard Layout|Switch to Last-Used Keyboard Layout|Switch keyboard layout to .*"))},
     {QStringLiteral("/component/kcm_touchpad"), QRegularExpression(QStringLiteral("Toggle Touchpad|Enable Touchpad|Disable Touchpad"))},
     {QStringLiteral("/component/kwin"), QRegularExpression(QStringLiteral("view_zoom_in|view_zoom_out|view_actual_size"))},
 };
